68cb64fc03a1d6f8bbac6cd94512a01cfdef06ab
artem <art.holodilov@gmail.com>
2026-04-28 18:47:55 +0300

    Connected auth plugin to ktor application

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,13 @@
 package com.example
 
+import com.example.plugin.configureJWT
 import io.ktor.server.engine.*
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureJWT()
 }
